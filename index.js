acb5f301aa8a6a3fd3ddfbfbdda345208fea64c0
Justine Rey Daquis <justinedaquis28@gmail.com>
2025-02-13 23:58:08 +0800

    final

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-    document.getElementById('yes').addEventListener('click', function () {
+document.getElementById('yes').addEventListener('click', function () {
     window.location.href = 'yes.html';
-    });
+});
 
-    document.getElementById('no').addEventListener('click', function () {
-    window.location.href = 'yes.html';
-    });
+document.getElementById('no').addEventListener('click', function () {
+    window.location.href = 'https://example.com'; // Change this URL to the desired destination
+});
 
-    var audio = new Audio('music.mp3');
-    document.getElementById('play').addEventListener('click', function () {
-        console.log("playing!") 
-        audio.play();
-    });
+var audio = new Audio('music.mp3');
+document.getElementById('play').addEventListener('click', function () {
+    console.log("playing!");
+    audio.currentTime = 50; // Start the audio at 50 seconds
+    audio.play();
+});
